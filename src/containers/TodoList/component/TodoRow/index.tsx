0f95ee14f738e61ee7c346b3a86d7b88ca8c957f
f-koki <fukushimakoki0108@gmail.com>
2020-06-26 22:10:53 +0900

    feat

--- src/containers/TodoList/component/TodoRow/index.tsx
+++ src/containers/TodoList/component/TodoRow/index.tsx
@@ -7,7 +7,6 @@ type Props = {
 const TodoRow: React.FC<Props> = ({ todo }) => (
   <div className="Todo">
     <div className="todo-text">{todo}</div>
-    {/* TODO: checkboxをクリックしたらtodo削除 */}
     <div className="toggle-button" onClick={() => {}}></div>
   </div>
 );
